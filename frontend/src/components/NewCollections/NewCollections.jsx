de8c1de2a5b121f65bc7d8d77cad5babcc234e91
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-01-27 17:11:55 +0530

    new collection component complete

---
+++ frontend/src/components/NewCollections/NewCollections.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './NewCollections.css'
+import new_collection from '../assets/new_collections'
+import Items from '../Items/Items'
+
+const NewCollections = () => {
+  return (
+    <div className='new-collections'>
+        <h1>NEW COLLECTIONS</h1>
+        <hr />
+        <div className="collections">
+            {
+              new_collection.map((item)=> <Items props={item} />)
+            }
+          
+        </div>
+    </div>
+  )
+}
+
+export default NewCollections
