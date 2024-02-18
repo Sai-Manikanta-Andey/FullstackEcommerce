c706c75c924eb4c92150900e51541b0452aaea82
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-02-18 16:15:56 +0530

    login endpoint complete

--- backend/index.js
+++ backend/index.js
@@ -175,6 +175,28 @@ app.post('/signup',async (req,res)=>{
   res.json({ success: true, token });
 })
 
+//creating endpoint for user login
+app.post('/login',async (req,res)=>{
+  let user = await Users.findOne({email:req.body.email})
+  if(user){
+    const passCompare = req.body.password === user.password
+    if(passCompare){
+      const data = {
+        user:{
+          id:user.id
+        }
+      }
+      const token = jwt.sign(data,'secret_ecom')
+      res.json({success:true,token})
+    }
+    else{
+      res.json({success:false,error:"wrong password"})
+    }
+  }else{
+    res.json({success:false,error:"Wrong email Id"})
+  }
+})
+
 app.listen(port,async (error) => {
   if (!error) {
     console.log("Server running on port" + port);
