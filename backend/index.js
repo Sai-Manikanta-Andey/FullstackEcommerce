349a6868fb91a62daf091ca813ec9aa3135930eb
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-02-18 10:24:08 +0530

    user schema & signup endpoint complete

--- backend/index.js
+++ backend/index.js
@@ -125,10 +125,61 @@ app.get("/allproducts", async (req, res) => {
   res.send(products);
 });
 
-app.listen(port, (error) => {
+//Scehma for user model
+const Users = mongoose.model("Users", {
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  cartData: {
+    type: Object,
+  },
+  date: {
+    type: Date,
+    default:Date.now
+  },
+});
+
+//Creating an endpoint for user
+app.post('/signup',async (req,res)=>{
+  let check = await Users.findOne({email:req.body.email})
+  if(check){
+    return res.status(400).json({success:false,error:'User already exist'})
+  }
+  let cart = {};
+
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+
+  await user.save();
+
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
+})
+
+app.listen(port,async (error) => {
   if (!error) {
     console.log("Server running on port" + port);
   } else {
     console.log("error : " + error);
   }
+  
 });
