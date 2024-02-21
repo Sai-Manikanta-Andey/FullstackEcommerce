c6c4f15c9f081025753b12ab2dc818a7a4ae7bab
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2024-02-21 19:46:15 +0530

    addtocart endpoint created and integrated

--- backend/index.js
+++ backend/index.js
@@ -218,6 +218,31 @@ app.get("/popularinwomen", async (req, res) => {
   res.send(popular_in_women);
 });
 
+//creating middleware to fetch user
+const fetchUser= async (req,res,next)=>{
+  const token = req.header('auth-token')  
+  if(!token){
+    res.status(401).send({error:"Please authenticate using valid token"})
+  }else{
+    try{
+      const data =jwt.verify(token,"secret_ecom")
+      req.user = data.user;
+      next()
+    }catch(error){
+      res.status(401).send({error:'Please authenticate using a valid token'})
+    }
+  }
+}
+
+
+//creating an endpoint for adding products to cart 
+app.post('/addtocart',fetchUser,async (req,res)=>{
+  let userData = await Users.findOne({_id:req.user.id})
+  userData.cartData[req.body.itemId]+=1
+  await Users.findByIdAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+  res.send("Added ")
+})
+
 app.listen(port,async (error) => {
   if (!error) {
     console.log("Server running on port" + port);
